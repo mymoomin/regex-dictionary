a870ed1d721ffdd1da5b3433fd4c10004353e268
mymoomin <baird_aidan@outlook.com>
2020-12-02 02:12:41 +0000

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react"
+import words from "../components/words"
+
+
+export default function Home() {
+  const [regex, setRegex] = useState(new RegExp("s"))
+
+  function updateRegex(e) {
+    let reg;
+    try {
+      reg = new RegExp(e.target.value)
+    } catch {
+      reg = regex
+    }
+    setRegex(reg)
+    console.log(regex)
+  }
+  
+  function matchRegex(inp) {
+    return regex.test(inp)
+  }
+
+  return (<div>
+    <h1>Regex Dictionary</h1>
+    <label htmlFor="regex">Enter regex here: </label>
+    <input name="regex" type="text" onChange={updateRegex} placeholder="Enter Regex" />
+
+    <h2>Words</h2>
+    <ol>
+      {words.map((value, index) => <li key={index} style={{display: matchRegex(value) || "none"}}>{value}</li>)}
+    </ol>
+  </div>)
+}
+
+
+
+
